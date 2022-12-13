cf8878cc2e2f1b435e85117e221e957134824198
JayCloudSandbox <107244812+JayCloudSandbox@users.noreply.github.com>
2022-12-13 12:31:32 +0000

    Create SgCreate.py

---
+++ SgCreate.py
@@ -0,0 +1,33 @@
+import sys,os
+import openpyxl
+import shotgun_api3
+
+class SeqGridCreate():
+    def __init__(self,serverPth,scNm,scKey,prjId,seq):
+        self.serverPath = serverPth
+        self.scriptName = scNm
+        self.scriptKey = scKey
+        self.projectId = prjId
+        self.seqCode = seq
+            
+    def runSeqCreate(self):
+        SERVER_PATH = self.serverPath
+        SCRIPT_NAME  = self.scriptName
+        SCRIPT_KEY = self.scriptKey
+        prjId = int(self.projectId)
+        seq = self.seqCode
+        sg = shotgun_api3.Shotgun(SERVER_PATH, SCRIPT_NAME, SCRIPT_KEY)
+
+        print("\nstarted creating assets ")
+        print("craeted {} ".format(seq))
+
+        data = {
+            "project": {"type": "Project", "id": prjId},
+            "code": seq,
+        }
+        sg.create('Sequence', data)
+
+        print("\ncompleted creating assets")
+
+if __name__ == '__main__':
+    SeqGridCreate("https://some.shotgrid.autodesk.com/","test_create_shot","Thnctx1vczmqbiq",716,"RT_T001").runSeqCreate()
